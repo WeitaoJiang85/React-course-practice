b895ef97e103a36d58485949306496677e0dc253
WEITAO JIANG <weitaoj@outlook.com>
2022-09-13 14:52:22 +1000

    delete files

--- lab05_giphy_translate/src/GifCard.js
+++
@@ -1,9 +0,0 @@
-const GifCard = ({ title, url }) => {
-  return (
-    <div className="card">
-      <h3>{title} </h3>
-      <img src={url} alt={title} />
-    </div>
-  )
-}
-export default GifCard
